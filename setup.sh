50846f5e6c9c3fcc8ae15d339e083094a51acbff
Aman Raj <amanraj68bittu@gmail.com>
2025-04-21 04:17:44 -0400

    Add PermisionV0.2.0

--- setup.sh
+++ setup.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 git config core.hooksPath .githooks
-
-# This script compiles and runs a C++ program.
-# SRC_FILE="src/main.cpp"
-# OUT_FILE="main"
-# g++ -std=c++17 "$SRC_FILE" -o "$OUT_FILE" # compile
-# # Run
-# if [ $? -eq 0 ]; then
-#     echo "✅ Compilation successful. Running program..."
-#     echo "-----------------------------"
-#     ./"$OUT_FILE"
-#     echo "-----------------------------"
-# else
-#     echo "❌ Compilation failed."
-# fi
+chmod +x ./.githooks/pre-push
+chmod +x ./test01.sh
+chmod +x ./runServer.sh
+chmod +x ./run.sh
